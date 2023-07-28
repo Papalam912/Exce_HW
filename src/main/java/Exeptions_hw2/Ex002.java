0d209f31e944744ee16dabf6aa5bc66e2590ddf8
SergeiShilkoysky <124188438+SergeiShilkoysky@users.noreply.github.com>
2023-07-28 22:40:37 +0300

    hw2

--- src/main/java/Exeptions_hw2/Ex002.java
+++ src/main/java/Exeptions_hw2/Ex002.java
@@ -1,11 +1,20 @@
 package Exeptions_hw2;
 
+
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-/*Если необходимо, исправьте данный код
-(задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit)
+/* 2.Если необходимо, исправьте данный код
+(задание 2 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit):
+try {
+    int d = 0;
+    double catchedRes1 = intArray[8] / d;
+    System.out.println("catchedRes1 = " + catchedRes1);
+} catch (ArithmeticException e) {
+    System.out.println("Catching exception: " + e);
+}
 */
+
 public class Ex002 {
     public static void main(String[] args) {
         testEsception();
--- src/main/java/Exeptions_hw2/Ex003.java
+++ src/main/java/Exeptions_hw2/Ex003.java
@@ -1,7 +1,7 @@
 package Exeptions_hw2;
 
-/*Дан следующий код, исправьте его там, где требуется
-(задание 3 https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit) */
+/* 3. Дан следующий код, исправьте его там, где требуется
+(https://docs.google.com/document/d/17EaA1lDxzD5YigQ5OAal60fOFKVoCbEJqooB9XfhT7w/edit) */
 //    public static void main(String[] args) throws Exception {
 //        try {
 //            int a = 90;
